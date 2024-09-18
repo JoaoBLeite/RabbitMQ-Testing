39182c2372dc4d4dfc2fe11e3f47405e72b6fca5
JoaoBLeite <joao222barbosa@gmail.com>
2024-09-18 11:38:48 -0300

    Project foundation

---
+++ demo-backend-notification-worker/src/main/java/com/jsd/demo_backend_notification_worker/DemoBackendNotificationWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.jsd.demo_backend_notification_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBackendNotificationWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBackendNotificationWorkerApplication.class, args);
+	}
+
+}
---
+++ demo-backend-notification-worker/src/test/java/com/jsd/demo_backend_notification_worker/DemoBackendNotificationWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsd.demo_backend_notification_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBackendNotificationWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
